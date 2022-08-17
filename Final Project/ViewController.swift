a7acda3e31dcd2118b48147dd9e7a18be6781384
aishani1604 <84450159+aishani1604@users.noreply.github.com>
2022-08-17 13:13:54 -0500

    clear button

--- Final Project/ViewController.swift
+++ Final Project/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textOne: UITextField!
+    @IBOutlet weak var textTwo: UITextField!
+    @IBOutlet weak var textThree: UITextField!
+    @IBOutlet weak var textFour: UITextField!
+    @IBOutlet weak var textFive: UITextField!
+    @IBOutlet weak var textSix: UITextField!
+    @IBOutlet weak var textSeven: UITextField!
+    @IBOutlet weak var textEight: UITextField!
+    @IBOutlet weak var textNine: UITextField!
+    @IBOutlet weak var textTen: UITextField!
+    @IBOutlet weak var textEleven: UITextField!
+    @IBOutlet weak var textTwelve: UITextField!
+    @IBOutlet weak var textThirteen: UITextField!
+    @IBOutlet weak var textFourteen: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        textOne.text = " "
+        textTwo.text = " "
+        textThree.text = " "
+        textFour.text = " "
+        textFive.text = " "
+        textSix.text = " "
+        textSeven.text = " "
+        textEight.text = " "
+        textNine.text = " "
+        textTen.text = " "
+        textEleven.text = " "
+        textTwelve.text = " "
+        textThirteen.text = " "
+        textFourteen.text = " "
+    }
+    
 
 }
 
